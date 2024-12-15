60105d510853d6243701df1b4ba11371ae319942
Aditya1or0 <adityapandit264@gmail.com>
2024-12-15 22:43:06 +0530

    .

--- server/server.js
+++ server/server.js
@@ -14,9 +14,7 @@ const allowedOrigins = ["http://localhost:5173"];
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({ origin: allowedOrigins, method: ["GET", "POST"], credentials: true })
-);
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 //Api Endpoints
 app.get("/", (req, res) => {
